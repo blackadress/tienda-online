faaa06628a432f95d0ea4d11663076b9f5303f95
blackadress <blackadress.01@gmail.com>
2021-10-17 20:52:05 -0500

    documentacion

--- gui/index.js
+++ gui/index.js
@@ -47,21 +47,31 @@ window.onload = () => {
   });
 };
 
-// guardar los datos en LocalStorage
-// disminuir la carga al servidor
+/** Funcion para guardar los datos en LocalStorage / disminuir carga de servidor
+  * @param {string} key La llave con la cual se va a guardar en LocalStorage
+  * @param {any} data La data que queremos guardar el LocalStorage
+  * @returns {void}
+  */
 const saveToLS = (key, data) => {
   localStorage.setItem(key, JSON.stringify(data));
 };
 
-// get data from LocalStorage if exists
+/** get data from LocalStorage if exists
+  * @param {string} key La llave que queremos recuperar de LocalStorage
+  * @returns {Array<boolean, Array<T>>} Si se pudo recuperar la data regresa [true, Array] caso contrario [false, []]
+  */
 const getFromLS = (key) => {
   const data = localStorage.getItem(key);
   if (data === null) {
-    return [false, 0];
+    return [false, []];
   }
   return [true, JSON.stringify(data)];
 };
 
+/** Renderiza las categorias que obtiene del servidor
+  * @param {Array} categories Un array de las categorias que queremos renderizar en forma de 'options'
+  * @return {void} Se renderizan las categorias en un elemento con id 'categories-root'
+  */
 const renderCategories = (categories) => {
   document.getElementById("categories-root").innerHTML = "";
   if (categories.length > 0) {
@@ -76,6 +86,10 @@ const renderCategories = (categories) => {
   }
 };
 
+/** Genera el html necesario para la renderizacion de objetos de tipo categoria
+  * @param {Object} category Un objecto categoria
+  * @returns {string} Se forma html con el tag 'option'
+  */
 const categoryOptionComp = (category) => {
   const { id, name } = category;
   const html = `
@@ -85,6 +99,9 @@ const categoryOptionComp = (category) => {
   return html;
 };
 
+/** Renderiza un gif de 'carga' en el elemento con id 'products-root'
+  * @returns {void}
+  */
 const productsLoading = () => {
   document.getElementById("products-root").innerHTML = `
     <img 
@@ -94,6 +111,10 @@ const productsLoading = () => {
   `;
 };
 
+/** Renderiza los productos que obtiene del servidor
+  * @param {Array} products Un array de los productos que queremos renderizar en forma de 'options'
+  * @return {void} Se renderizan los productos en un elemento con id 'products-root'
+  */
 const renderProducts = (products) => {
   document.getElementById("products-root").innerHTML = "";
   if (products.length > 0) {
@@ -108,6 +129,10 @@ const renderProducts = (products) => {
   }
 };
 
+/** Genera el html necesario para la renderizacion de objetos de tipo producto
+  * @param {Object} producto Un objecto de tipo producto
+  * @returns {string} Se forma una `card` html
+  */
 const productComp = (product) => {
   const { name, price, discount } = product;
   const url_image = product.url_image
@@ -156,26 +181,44 @@ const productComp = (product) => {
 const URL = "https://pruebasupergenial.herokuapp.com";
 // const URL = "http://localhost:4000";
 
+/** Obtener todos los productos de la API
+  * @returns {Promise<Product[]>}
+  */
 const getProducts = () => {
   const url = `${URL}/products`;
   return getRequest(url);
 };
 
+/** Obtener los productos filtrados por nombre de la API
+  * @param {string} name La cadena de texto que se va a buscar
+  * @returns {Promise<Product[]>}
+  */
 const getProductsByName = (name) => {
   const url = `${URL}/products/search/name/${name}`;
   return getRequest(url);
 };
 
+/** Obtener los productos filtrados por categoria de la API
+  * @param {number} categoryId ID de la categoria por la cual se va a filtrar
+  * @returns {Promise<Product[]>}
+  */
 const getProductsByCategory = (categoryId) => {
   const url = `${URL}/products/category/${categoryId}`;
   return getRequest(url);
 };
 
+/** Obtener todas las categorias desde la API
+  * @returns {Promise<Category[]>}
+  */
 const getCategories = () => {
   const url = `${URL}/categories`;
   return getRequest(url);
 };
 
+/** Mandar una request con el metodo GET
+  * @params {string} url La URL a la cual se le va a mandar la request
+  * @return {Promise<any>}
+  */
 const getRequest = (url) => {
   return fetch(url)
     .then(res => {
